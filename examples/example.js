0c25ee6fdf038ad012802f02691eab55345c0e7f
Leo Long <yujunlong2000@gmail.com>
2018-05-04 18:11:50 +0800

    perfect for mount by prefix

--- examples/example.js
+++ examples/example.js
@@ -1,17 +1,19 @@
 const Logoran = require('logoran');
 const logoranSwagger = require('../lib/');
 const router = require('logoran-router')();
+const mount = require('logoran-mount');
 
 const app = new Logoran();
 module.exports = app;
 
 app.use(logoranSwagger());
 
-router.get('/moredocs', logoranSwagger({ routePrefix: false }));
-
+router.get('/moredocs', logoranSwagger({ routePrefix: false, favicon16: '/unexisted16.png', favicon32: '/unexisted32.png' }));
 app
   .use(router.routes())
   .use(router.allowedMethods());
+
+app.use(mount('/mountdocs', logoranSwagger({ routePrefix: '/mountdocs' })));
 
 /* istanbul ignore if */
 if (!module.parent) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -9,19 +9,8 @@ describe('logoran-swagger-ui', function() {
     return request(app.callback())
       .get('/docs')
       .expect('Content-Type', /html/)
-      .expect(200);
-  });
-  it('should return index file from logoran router', function() {
-    return request(app.callback())
-      .get('/moredocs')
-      .expect('Content-Type', /html/)
-      .expect(200);
-  });
-  it('should return css', function() {
-    const url = `https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/${json.devDependencies['swagger-ui-dist']}`;
-    return request(url)
-      .get('/swagger-ui.css')
-      .expect('Content-Type', 'text/css')
+      .expect(/href="\/docs\/favicon-16x16\.png" sizes="16x16"/)
+      .expect(/href="\/docs\/favicon-32x32\.png" sizes="32x32"/)
       .expect(200);
   });
   it('should return icon16x16', function() {
@@ -34,6 +23,41 @@ describe('logoran-swagger-ui', function() {
     return request(app.callback())
       .get('/favicon-32x32.png')
       .expect('Content-Type', /png/)
+      .expect(200);
+  });
+  it('should return index file from logoran router', function() {
+    return request(app.callback())
+      .get('/moredocs')
+      .expect('Content-Type', /html/)
+      .expect(/href="\/unexisted16\.png" sizes="16x16"/)
+      .expect(/href="\/unexisted32\.png" sizes="32x32"/)
+      .expect(200);
+  });
+  it('should return index file from logoran router', function() {
+    return request(app.callback())
+      .get('/mountdocs')
+      .expect('Content-Type', /html/)
+      .expect(/href="\/mountdocs\/favicon-16x16\.png" sizes="16x16"/)
+      .expect(/href="\/mountdocs\/favicon-32x32\.png" sizes="32x32"/)
+      .expect(200);
+  });
+  it('should return icon16x16', function() {
+    return request(app.callback())
+      .get('/mountdocs/favicon-16x16.png')
+      .expect('Content-Type', /png/)
+      .expect(200);
+  });
+  it('should return icon32x32', function() {
+    return request(app.callback())
+      .get('/mountdocs/favicon-32x32.png')
+      .expect('Content-Type', /png/)
+      .expect(200);
+  });
+  it('should return css', function() {
+    const url = `https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/${json.devDependencies['swagger-ui-dist']}`;
+    return request(url)
+      .get('/swagger-ui.css')
+      .expect('Content-Type', 'text/css')
       .expect(200);
   });
 });
